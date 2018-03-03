8394ae18f39753e1f9a7f0724eb0400c06feae42
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-03-03 23:03:22 +0000

    Add short version pattern matching, fix struct syntax

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,8 +6,7 @@ defmodule Identicon do
   end
 
   def pick_color(image) do
-    %Identicon.Image{hex: hex_list} = image
-    [r, g, b | _tail] = hex_list
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
     [r, g, b]
   end
 
@@ -15,7 +14,7 @@ defmodule Identicon do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
-    %Identicon.Image(hex: hex)
+    %Identicon.Image{hex: hex}
   end
 
 end
