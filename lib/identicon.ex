facdb85ac8c14f0da254d19f9a6e00463d7be7db
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-03-03 22:53:20 +0000

    Add pick_color, takes first 3 numbers from array

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,12 @@ defmodule Identicon do
     |> hash_input
   end
 
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+    [r, g, b]
+  end
+
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
