f54828962067011e3cff2ece957c6e391b52cd48
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-03-03 22:20:10 +0000

    Add Hash string with md5

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,11 +1,13 @@
 defmodule Identicon do
 
   def main(input) do
-    
+    input
+    |> hash_input
   end
 
   def hash_input(input) do
-    
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 
 end
