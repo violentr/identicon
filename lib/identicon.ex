77e91edc7ae28c2a293c64961a6ec4726ab45514
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-03-05 23:11:51 +0000

    Add filter_odd_squares
    Remove tuple from the list if the number is odd.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,25 +5,7 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
-  end
-  
-  def build_grid(%Identicon.Image{hex: hex}=image) do
-    grid = 
-      hex
-      |> Enum.chunk(3)
-      |> Enum.map(&mirror_row/1)
-      |> List.flatten
-      |> Enum.with_index
-    %Identicon.Image{image | grid: grid}
-  end
-  
-  def mirror_row(row) do
-    [first, second | _tail] = row
-    row ++ [second, first]
-  end
-
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    %Identicon.Image{image | color: {r, g, b}}
+    |> filter_odd_squares
   end
 
   def hash_input(input) do
@@ -33,4 +15,29 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex}=image) do
+    grid = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Identicon.Image{image | grid: grid}
+  end
+    
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid}=image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+    %Identicon.Image{image | grid: grid}
+  end
 end
