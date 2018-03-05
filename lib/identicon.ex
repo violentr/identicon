8039966bd044bb649526fece003e0db6375e25bb
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-03-05 23:32:29 +0000

    Add build_pixel_map as struct component
    method calculates x and y for top_left
    and x y for bottom_right coordinates
    returns as a part or Identicon.Image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,7 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
   end
 
   def hash_input(input) do
@@ -39,5 +40,16 @@ defmodule Identicon do
       rem(code, 2) == 0
     end
     %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid}=image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      top_left = {horizontal, vertical}
+      botton_right = {horizontal + 50 , vertical + 50}
+      {top_left, botton_right}
+    end
+    %Identicon.Image{image| pixel_map: pixel_map}
   end
 end
