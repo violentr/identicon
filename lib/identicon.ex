2a7aecd40509e53b215ca390fc94198449b48eec
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-03-06 00:07:54 +0000

    Add save_image, binary file saved to the disk

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,6 +8,7 @@ defmodule Identicon do
     |> filter_odd_squares
     |> build_pixel_map
     |> draw_image
+    |> save_image(input)
   end
 
   def hash_input(input) do
@@ -62,5 +63,8 @@ defmodule Identicon do
     end
     :egd.render(image)
   end
-
+  
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
 end
